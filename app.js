a43be6a744ad7ced86c0ee58b3c7de5806c5c1d9
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-01-23 08:57:59 +0100

    feat(multer) add multer require

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let cors = require('cors');
+let multer = require('multer'); // enctype="multipert/form-data"
+
+let storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null,"public/upload");
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
+    
+})
+
+const upload =  multer({storage}); // upload.single=""   / src="/upload/${req.file.filename}"
 
 
 
@@ -11,6 +24,7 @@ let app = express();
 
 let userRouter = require('./routes/users');
 let loginRouter = require('./routes/login');
+let imageRouter
 
 
 app.use(cors());

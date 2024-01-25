e6fc8b07a7b8414a02a94cc4916b7993f1b8c3cc
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-01-25 08:36:26 +0100

    feat(image add image

--- routes/users.js
+++ routes/users.js
@@ -6,24 +6,24 @@ let { randomUUID } = require('crypto');
 let cryptoJS = require('crypto-js');
 
 
-let multer = require('multer'); // enctype="multipert/form-data"
+let multer = require('multer'); 
+
 
 let storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null,"public/upload");
+        cb(null,"public/upload/userimage");
     },
     filename: function(req, file, cb){
         cb(null, file.originalname);
     }
-    
 })
 
-const upload =  multer({storage}); // upload.single=""   / src="/upload/${req.file.filename}"
+const upload =  multer({storage}); 
 
-
+// middleware
 router.use(cors());
        
-
+// Chech if user exist
 router.post('/check', (req, res) => {
   fs.readFile('usersInfo.json', function(err, data){
     if(err){
@@ -42,27 +42,15 @@ router.post('/check', (req, res) => {
   })
 })
 
+// add image to user
 router.post('/image', upload.single("image"), (req,res) => {
-  const userImage = req.file.filename;
-  console.log(req.body)
 
-  fs.readFile('usersInfo.json', (err, data) => {
-    if(err){
-      console.log(err)
-    }
-
-    const userInfo = JSON.parse(data);
-
-    const addImage = {
-      "imageSrc": userImage
-    }
-
-
-  })
-
+res.json('ok')
+console.log(req.body)
 
 })
 
+// add new user
 router.post('/add', (req, res) => {
 
     fs.readFile('usersInfo.json', function(err,data){
@@ -74,7 +62,8 @@ router.post('/add', (req, res) => {
             id: randomUUID(),
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: req.body.password,
+            userImage: false
         }
 
         fs.writeFile('usersInfo.json', JSON.stringify(userInfo,null, 2), function(err){
@@ -99,10 +88,11 @@ router.post('/add', (req, res) => {
          id: randomUUID(),
          name: req.body.name,
          email: req.body.email,
-         password: cryptoPassWord
+         password: cryptoPassWord,
+         userImage: false
      }
 
-    userInfo.push(user)
+    userInfo.push(user);
     
     fs.writeFile('usersInfo.json', JSON.stringify(userInfo,null, 2), function(err){
         if (err){
